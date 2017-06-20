f94be927c92d8628e3923ce4e60db1d504abe81b
zxfonline <zxfonline@sina.com>
2017-06-20 16:30:27 +0800

    优化
    优化ip过滤

--- trace.go
+++ trace.go
@@ -12,9 +12,10 @@ import (
 	"golang.org/x/net/trace"
 )
 
-func Init(enableTracing bool) {
+func Init(enableTracing bool, checkip bool) {
+	iptable.CHECK_IPTRUSTED = checkip
 	trace.AuthRequest = func(req *http.Request) (any, sensitive bool) {
-		w := iptable.IsTrustedIP(iptable.GetRemoteAddrIP(req.RemoteAddr))
+		w := iptable.IsTrustedIP(iptable.GetRemoteAddrIP(req.RemoteAddr), false)
 		return w, w
 	}
 	EnableTracing = enableTracing
@@ -29,3 +30,42 @@ func Init(enableTracing bool) {
 
 // EnableTracing controls whether to trace using the golang.org/x/net/trace package.
 var EnableTracing = true
+
+//ProxyTrace 跟踪
+type ProxyTrace struct {
+	tr trace.Trace
+}
+
+//TraceStart 开始跟踪
+func TraceStart(family, title string) *ProxyTrace {
+	if EnableTracing {
+		pt := &ProxyTrace{tr: trace.New(family, title)}
+		return pt
+	}
+	return nil
+}
+
+func TraceFinish(pt *ProxyTrace) {
+	if pt != nil {
+		if pt.tr != nil {
+			pt.tr.Finish()
+		}
+	}
+}
+
+func TracePrintf(pt *ProxyTrace, format string, a ...interface{}) {
+	if pt != nil {
+		if pt.tr != nil {
+			pt.tr.LazyPrintf(format, a...)
+		}
+	}
+}
+
+func TraceErrorf(pt *ProxyTrace, format string, a ...interface{}) {
+	if pt != nil {
+		if pt.tr != nil {
+			pt.tr.LazyPrintf(format, a...)
+			pt.tr.SetError()
+		}
+	}
+}
